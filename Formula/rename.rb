71d4fa1b87cd9265aaf0fc615a92cbad25592c58
goreleaserbot <goreleaser@carlosbecker.com>
2019-04-28 13:37:05 +0200

    Brew formula update for rename version v0.1.3

--- Formula/rename.rb
+++ Formula/rename.rb
@@ -2,9 +2,9 @@
 class Rename < Formula
   desc "Unix Rename command, written in Go"
   homepage "https://github.com/fberge/rename"
-  url "https://github.com/fbergen/rename/releases/download/v0.1.2/rename_0.1.2_Darwin_x86_64.tar.gz"
-  version "0.1.2"
-  sha256 "88443eadb07bda3d2d4cfd2713defd962a830671efdf3edba26e77bb1e514de0"
+  url "https://github.com/fbergen/rename/releases/download/v0.1.3/rename_0.1.3_Darwin_x86_64.tar.gz"
+  version "0.1.3"
+  sha256 "ac060766cc4161c7bbcefb33adc7be21b74efdafad0e3a53667461855769ac8c"
 
   def install
     bin.install "rename"
